379e561d2c241574e151774e838569ecbb587c7b
Robert J. Mitchell <robert.judson@outlook.com>
2019-12-06 11:28:08 -0500

    Header complete

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,25 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+  //create the elements
+  const headerContainer = document.createElement('div');
+  const date = document.createElement('span');
+  const headerOne = document.createElement('h1');
+  const temp = document.createElement('span');
+  
+  //add classes to the elements
+  headerContainer.classList.add('header');
+  date.classList.add('date');
+  temp.classList.add('temp');
+
+  //append the children
+  headerContainer.appendChild(date);
+  headerContainer.appendChild(headerOne);
+  headerContainer.appendChild(temp);
+
+  return headerContainer;
+}
+
+const parentHeader = document.querySelector('.header-container');
+parentHeader.appendChild(Header());
